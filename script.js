39cdf31611021fe5c315dce059d3462b6d169da3
puja110 <csta.puja@gmail.com>
2024-01-31 00:17:40 -0500

    adding comments in pokemon listing funtionalities

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const pokemonURL = "https://pokeapi.co/api/v2/pokemon?limit=5";
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    // fetching pokemon data
     const getPokemonData = () => {
-        // fetching pokemon data
         fetch(pokemonURL)
             .then(response => response.json())
             .then(data => {
+                // extracting results data from pokemon response
                 const listOfPokemon = data.results;
+
+                // adding item to pokemon list
                 addPokemonItem(listOfPokemon);
             });
     };
@@ -16,13 +20,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // displaying pokemon image and name for each item
         data.forEach(item => {
+            // creating div and text element for pokemon item
             const pokemonItem = document.createElement('div');
             const pokemonName = document.createElement('p');
+
+            // getting pokemon ID
             const pokemonID = item.url.split('/')[6];
+
+            // applying styling to pokemon item
             pokemonItem.className = 'pokemon-item';
+
+            // getting and setting pokemon image and name
             pokemonItem.innerHTML = `
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png" alt="${item.name}" class="pokemon-image">`;
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png" 
+                    alt="${item.name}" class="pokemon-image">`;
             pokemonName.textContent = item.name;
+
+            // appending pokemon items to pokemon list element
             pokemonItem.appendChild(pokemonName);
             pokemonListElement.appendChild(pokemonItem);
         });
