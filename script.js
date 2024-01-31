5d16a299b384fcd5039d6b3318ab0afee28a667d
puja110 <csta.puja@gmail.com>
2024-01-31 00:09:59 -0500

    5 pokemon item added in the list with their image and name

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const baseURL = "https://pokeapi.co/api/v2/pokemon";
+const pokemonURL = "https://pokeapi.co/api/v2/pokemon?limit=5";
 
-fetch(baseURL)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("pokemon data: ", data)
-    })
-    .catch((error) => console.error("error: ", error))
+document.addEventListener("DOMContentLoaded", () => {
+    const getPokemonData = () => {
+        // fetching pokemon data
+        fetch(pokemonURL)
+            .then(response => response.json())
+            .then(data => {
+                const listOfPokemon = data.results;
+                addPokemonItem(listOfPokemon);
+            });
+    };
+
+    function addPokemonItem(data) {
+        const pokemonListElement = document.getElementById("pokemonListElement");
+
+        // displaying pokemon image and name for each item
+        data.forEach(item => {
+            const pokemonItem = document.createElement('div');
+            const pokemonName = document.createElement('p');
+            const pokemonID = item.url.split('/')[6];
+            pokemonItem.className = 'pokemon-item';
+            pokemonItem.innerHTML = `
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png" alt="${item.name}" class="pokemon-image">`;
+            pokemonName.textContent = item.name;
+            pokemonItem.appendChild(pokemonName);
+            pokemonListElement.appendChild(pokemonItem);
+        });
+    }
+    
+    getPokemonData();
+});
