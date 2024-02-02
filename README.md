cccf26c8490b56f81ea81c8f5f5fbd2c360e9e0d
Suyog Shrestha <suyogshrestha.tech@gmail.com>
2024-02-02 10:39:05 -0500

    Proper Folder Structure Updated

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MDEV1004-Assignment1-Pokemon
+
 Group Assignment implementing RestAPI functionality, Internal Documentation, Version Control, Cloud Deployment, and Video Presentation.
+
+## Folder Structure \n
+
+css
+
+- style.css
+  js
+- script.js
+
+- index.html
--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@
  * Date: Feb 1st, 2024
  */
 
-const pokemonURL = 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=5';
-let currentURL = '';
+const pokemonURL = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=5";
+let currentURL = "";
 
 // Event listener for when the DOM content is loaded
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   /**
    * Sets the current URL in the UI
    * @param {string} url - The URL to be displayed
    */
   function setCurrentUrlBox(url) {
-    const urlBox = document.getElementById('url-box');
+    const urlBox = document.getElementById("url-box");
     urlBox.textContent = url;
   }
 
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   }
 
@@ -55,20 +55,20 @@ document.addEventListener('DOMContentLoaded', function () {
    * @param {Array} data - List of Pokemon data
    */
   function addPokemonItem(data) {
-    const pokemonListElement = document.getElementById('pokemonListElement');
-    const specsDetailSection = document.getElementById('specs-details');
-    const abilitiesDetailSection = document.getElementById('abilities-details');
-    specsDetailSection.style.display = 'none';
-    abilitiesDetailSection.style.display = 'none';
+    const pokemonListElement = document.getElementById("pokemonListElement");
+    const specsDetailSection = document.getElementById("specs-details");
+    const abilitiesDetailSection = document.getElementById("abilities-details");
+    specsDetailSection.style.display = "none";
+    abilitiesDetailSection.style.display = "none";
 
     data.forEach((item) => {
       // Creating div and text element for each Pokemon item
-      const pokemonItem = document.createElement('div');
-      const pokemonName = document.createElement('p');
-      pokemonName.classList.add('pokemon-name');
+      const pokemonItem = document.createElement("div");
+      const pokemonName = document.createElement("p");
+      pokemonName.classList.add("pokemon-name");
 
-      const pokemonID = item.url.split('/')[6];
-      pokemonItem.className = 'pokemon-item';
+      const pokemonID = item.url.split("/")[6];
+      pokemonItem.className = "pokemon-item";
 
       // Setting Pokemon image and name
       pokemonItem.innerHTML = `
@@ -77,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function () {
       pokemonName.textContent = item.name;
 
       // Appending Pokemon items to the list
-      pokemonItem.addEventListener('click', () => showPokemonDetails(item));
+      pokemonItem.addEventListener("click", () => showPokemonDetails(item));
       pokemonItem.appendChild(pokemonName);
       pokemonListElement.appendChild(pokemonItem);
     });
@@ -90,42 +90,42 @@ document.addEventListener('DOMContentLoaded', function () {
   async function showPokemonDetails(data) {
     setCurrentUrlBox(data.url);
     const pokemon = await getPokemonInfo(data.url);
-    const specsDetails = document.getElementById('specs-details');
+    const specsDetails = document.getElementById("specs-details");
 
     while (specsDetails.firstChild) {
       specsDetails.removeChild(specsDetails.firstChild);
     }
-    specsDetails.style.display = 'block';
+    specsDetails.style.display = "block";
 
     // Displaying Pokemon details
-    const name = document.createElement('p');
+    const name = document.createElement("p");
     name.innerHTML = `<b>Name:</b> ${pokemon.name}`;
     specsDetails.appendChild(name);
 
-    const height = document.createElement('p');
+    const height = document.createElement("p");
     height.innerHTML = `<b>Height:</b> ${pokemon.height}`;
     specsDetails.appendChild(height);
 
-    const baseExperience = document.createElement('p');
+    const baseExperience = document.createElement("p");
     baseExperience.innerHTML = `<b>Base Experience:</b> ${pokemon.base_experience}`;
     specsDetails.appendChild(baseExperience);
 
-    const abilities = document.createElement('p');
+    const abilities = document.createElement("p");
     abilities.innerHTML = `<b>Abilities:</b>`;
     specsDetails.appendChild(abilities);
 
     // Displaying Pokemon abilities
     pokemon.abilities.forEach(async (ability) => {
       const abilityData = await getPokemonInfo(ability.ability.url);
-      const abilityElement = document.createElement('div');
-      abilityElement.classList.add('ability');
-      abilityElement.innerHTML = 'Abilities: ';
+      const abilityElement = document.createElement("div");
+      abilityElement.classList.add("ability");
+      abilityElement.innerHTML = "Abilities: ";
       abilityElement.innerHTML = `
         <p>${ability.ability.name}, </p>
       `;
-      abilityElement.classList.add('abilities-text');
-      abilityElement.addEventListener('click', () =>
-        showAbilityDetails(abilityData, ability.ability.url)
+      abilityElement.classList.add("abilities-text");
+      abilityElement.addEventListener("click", () =>
+        showAbilityDetails(abilityData, ability.ability.url),
       );
       specsDetails.appendChild(abilityElement);
     });
@@ -138,28 +138,28 @@ document.addEventListener('DOMContentLoaded', function () {
    */
   function showAbilityDetails(ability, url) {
     setCurrentUrlBox(url);
-    const abilitiesDetail = document.getElementById('abilities-details');
-    abilitiesDetail.style.display = 'block';
+    const abilitiesDetail = document.getElementById("abilities-details");
+    abilitiesDetail.style.display = "block";
     while (abilitiesDetail.firstChild) {
       abilitiesDetail.removeChild(abilitiesDetail.firstChild);
     }
 
     // Displaying ability details
-    const abilityDetails = document.createElement('div');
-    abilityDetails.classList.add('ability-details');
+    const abilityDetails = document.createElement("div");
+    abilityDetails.classList.add("ability-details");
     abilityDetails.innerHTML = `
       <h4><b>Ability Name:</b> ${ability.name}</h4>
       <p><b>Ability Effect:</b> ${
-        ability.effect_entries.find((entry) => entry.language.name === 'en')
+        ability.effect_entries.find((entry) => entry.language.name === "en")
           .effect
       }</p>
       <p><b>Short Effect:</b> ${
-        ability.effect_entries.find((entry) => entry.language.name === 'en')
+        ability.effect_entries.find((entry) => entry.language.name === "en")
           .short_effect
       }</p>
       <p><b>Flavor Text:</b> ${
         ability.flavor_text_entries.find(
-          (entry) => entry.language.name === 'en'
+          (entry) => entry.language.name === "en",
         ).flavor_text
       }</p>
     `;
